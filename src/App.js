a8df8c0f02ae81ccbc923af38bcdc04d3d880193
nghianguyen119 <trongnghia.nguyen@outlook.com>
2022-02-22 02:13:15 +0100

    Upload files to IPFS gateway using urlSource

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import { useState } from "react";
-import { create } from "ipfs-http-client";
+import { create, urlSource } from "ipfs-http-client";
 import axios from "axios";
-import { BigNumber, ethers } from "ethers";
-import detectEthereumProvider from "@metamask/detect-provider";
-import { ABI } from "./abi";
-const contractAddress = "0xa0Afb3513B99E1b099CE9F3C007eE937B04e7870";
 const ipfsGateway = "https://gw.crustapps.net";
 // const ipfsGateway = "https://crustwebsites.net";
 // const ipfsGateway = "https://crustipfs.xyz";
@@ -27,36 +23,12 @@ export const pinW3Crust = async (authHeader, cid, name) => {
 };
 
 function App() {
-  const [authHeader, setAuthHeader] = useState();
-  const [file, setFile] = useState();
-  const [imageCid, setImageCid] = useState();
-  const [name, setName] = useState();
-  const [desc, setDesc] = useState();
-  const [metadataCid, setMetadataCid] = useState();
-  const [tokenId, setTokenId] = useState();
-  const [txHash, setTxHash] = useState();
-
-  const getSignatureHandle = async () => {
-    const provider = await detectEthereumProvider();
-    if (provider && provider.isMetaMask) {
-      const chainId = await provider.request({
-        method: "eth_chainId",
-      });
-
-      if (parseInt(chainId, 16) === 137) {
-        await provider.request({ method: "eth_requestAccounts" });
-        const providerPolygon = new ethers.providers.Web3Provider(provider);
-        const signer = providerPolygon.getSigner();
-        const addr = await signer.getAddress();
-        const signature = await signer.signMessage(addr);
-        setAuthHeader(
-          Buffer.from(`pol-${addr}:${signature}`).toString("base64")
-        );
-      }
-    }
-  };
+  const [authHeader, setAuthHeader] = useState(
+    "cG9sLTB4QTIyOGNGYWI4MEE2NzM4NTIyNDc2RGVDMTFkNzkzZDYxMjk5NjhiMjoweGU2ZDA1NDIzYTcxY2YzNjdjNWNhZmQwNzRmOWZjODAyMWUwMmEzZDA4MGViZTMyY2VhNDA0MjkwZTgxOWM5YTExMDUxMjNhZDJjZWM2ZjQ1Y2NiZWRmOTYyYjc5NzA4YWRiYjMwNTcxMGEzZWIzYjMzOWM3MzFmNTc1NGM4NWY1MWM="
+  );
 
   const uploadImageHandle = async () => {
+    console.log("start uploading");
     const ipfs = create({
       url: ipfsGateway + "/api/v0",
       headers: {
@@ -64,153 +36,33 @@ function App() {
       },
     });
 
-    const added = await ipfs.add(file);
-    pinW3Crust(authHeader, added.path, "image");
-    setImageCid(added.path);
-  };
-
-  const uploadMetadataHandle = async () => {
-    const ipfs = create({
-      url: ipfsGateway + "/api/v0",
-      headers: {
-        authorization: "Basic " + authHeader,
-      },
-    });
-
-    const metadata = {
-      name: name,
-      description: desc,
-      image: `ipfs://${imageCid}`,
+    const addOptions = {
+      pin: true,
+      wrapWithDirectory: true,
+      timeout: 10000,
     };
 
-    const added = await ipfs.add(JSON.stringify(metadata));
-    pinW3Crust(authHeader, added.path, "metadata");
-    setMetadataCid(added.path);
-  };
-
-  async function mintNTF() {
-    const provider = await detectEthereumProvider();
-    if (provider && provider.isMetaMask) {
-      const chainId = await provider.request({
-        method: "eth_chainId",
-      });
-
-      if (parseInt(chainId, 16) === 137) {
-        const providerPolygon = new ethers.providers.Web3Provider(provider);
-        const signer = providerPolygon.getSigner();
-        const addr = await signer.getAddress();
-        const contract = new ethers.Contract(
-          contractAddress,
-          ABI,
-          providerPolygon
-        );
-        const signedContract = contract.connect(signer);
-
-        signedContract
-          .mintDataNTF(
-            addr,
-            `ipfs://${metadataCid}`,
-            `ipfs://${imageCid}`,
-            "null",
-            { gasPrice: BigNumber.from("36000000000") }
-          )
-          .then((tx) => {
-            setTxHash(tx.hash);
-            providerPolygon.waitForTransaction(tx.hash).then(() => {
-              providerPolygon.getTransactionReceipt(tx.hash).then((receipt) => {
-                setTokenId(parseInt(receipt.logs[0].topics[3], 16));
-              });
-            });
-          })
-          .catch((error) => {});
-      }
+    for await (const file of ipfs.addAll(
+      [
+        urlSource(
+          "https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png"
+        ),
+        urlSource(
+          "https://thumbs.dreamstime.com/z/l-alphabet-marque-avec-des-lettres-l-abc-14509171.jpg"
+        ),
+      ],
+      addOptions
+    )) {
+      console.log(file.cid.toV0().toString());
     }
-  }
+  };
 
   return (
     <div style={{ paddingLeft: "50px" }}>
-      <h1>1. Get Signature</h1>
-      <button onClick={getSignatureHandle}>Get Signature</button>
-      <div>{authHeader}</div>
-
-      {/* <h1>2. Upload Image</h1>
-      <input
-        type="file"
-        onChange={(e) => {
-          setFile(e.target.files[0]);
-        }}
-      />
+      <h1>1. Upload Images</h1>
       <br />
       <button onClick={uploadImageHandle}>Upload</button>
       <br />
-      {imageCid && (
-        <a
-          href={`https://gw.crustapps.net/ipfs/${imageCid}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          File Link
-        </a>
-      )}
-
-      <h1>3. Upload Metadata</h1>
-      <div>Name</div>
-      <input
-        onChange={(e) => {
-          setName(e.target.value);
-        }}
-      />
-      <div>Description</div>
-      <input
-        onChange={(e) => {
-          setDesc(e.target.value);
-        }}
-      />
-      <br />
-      <button onClick={uploadMetadataHandle}>Upload</button>
-      <br />
-      {metadataCid && (
-        <a
-          href={`https://gw.crustapps.net/ipfs/${metadataCid}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Metadata Link
-        </a>
-      )}
-
-      <h1>4. Mint NFT</h1>
-      <button onClick={mintNTF}>Mint NFT</button>
-      <br />
-      {txHash && (
-        <a
-          href={`https://polygonscan.com/tx/${txHash}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Transaction Hash
-        </a>
-      )}
-      <br />
-      {tokenId && (
-        <a
-          href={`https://switchswap.io/#/assets/polygon/${contractAddress}/${tokenId}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          NFT on SwitchSwap
-        </a>
-      )}
-      <br />
-      {tokenId && (
-        <a
-          href={`https://opensea.io/assets/matic/${contractAddress}/${tokenId}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          NFT on OpenSea
-        </a>
-      )} */}
     </div>
   );
 }
